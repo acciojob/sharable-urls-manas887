9ac1f32b33dffa834ed8e81754cfbca4beacfac1
manas887 <116465782+manas887@users.noreply.github.com>
2023-02-24 23:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+const form = document.querySelector('form');
+      const url = document.querySelector('#url');
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const name = document.querySelector('#name').value;
+        const year = document.querySelector('#year').value;
+        if (name && year) {
+          const queryString = `?name=${name}&year=${year}`;
+          url.textContent = `https://localhost:8080/${queryString}`;
+        } else {
+          alert('Name and year cannot be empty');
+        }
+      });
